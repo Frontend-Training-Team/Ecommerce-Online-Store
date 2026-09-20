ecd26656592a1f3bb54e290d840a67d81a205728
Ahmed <ahmednage2024@gmail.com>
2026-09-20 10:12:44 +0300

    lolo

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,6 +1,21 @@
 function MyOrdersPage() {
     return (<>
-        <h1 className="text-2xl">MyOrdersPage</h1>
+        <div className="max-w-7xl mx-auto px-4 py-8 text-2xl bg-amber-200">
+            <h2 className="text-4xl font-Instrument">My Orders Page</h2>
+            <div className="max-w-7xl bg-[#DFDFDF]">
+                {/* left side */}
+                <div className="">
+                    <span>Delivered</span>
+                    <span>#12345678</span>
+                    <span>sep 3,2026</span>
+                </div>
+                {/* right side */}
+                <div className="">
+                    <span>$1,500.1</span>
+                    <span>4 item(s)</span>
+                </div>
+            </div>
+        </div>
     </>);
 }
 
