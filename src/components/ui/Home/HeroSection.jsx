45b777b01a71af52f4f569fde3e69843eade4267
Kareem Ahmed <kareem.ahmed09864@gmail.com>
2026-09-23 06:10:11 +0300

    ay 7aga

--- src/components/ui/Home/HeroSection.jsx
+++ src/components/ui/Home/HeroSection.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import heroVideo from "../../../assets/video/hero.mp4";
+import { Sparkles } from "lucide-react";
 
 function HeroSection({ onViewCategories }) {
   return (
@@ -18,9 +19,9 @@ function HeroSection({ onViewCategories }) {
 
       <div className="relative z-10 mx-auto flex min-h-230 max-w-7xl items-center justify-center px-6 py-16 text-center">
         <div className="max-w-4xl">
-          <div className="mx-auto mb-4 w-fit rounded-full bg-white/10 border border-white/50 px-5 py-2">
-          
-            <p className="text-white text-sm uppercase tracking-widest">
+          <div className="flex mx-auto mb-4 w-fit rounded-full items-center bg-white/10 border-0.5 border-white/15 px-5 py-2">
+            <Sparkles className="text-white h-3.25 w-3.25 mr-2"/>
+            <p className="text-white text-sm font-Inter">
               Premium Shopping Experience
             </p>
           </div>
@@ -29,13 +30,13 @@ function HeroSection({ onViewCategories }) {
             shop the future, delivered today
           </h1>
 
-          <p className="mx-auto mt-4 max-w-none whitespace-nowrap text-[10px] leading-6 text-[#E5DCD5] sm:text-base">
+          <p className="mx-auto mt-4 max-w-none whitespace-nowrap text-md font-Inter leading-6 text-white sm:text-base">
             Discover premium products at unbeatable prices. Fast delivery, easy returns, and exceptional quality.
           </p>
           <div className="mt-8 flex items-center justify-center gap-4">
             <Link
               to="/shop"
-              className="rounded-3xl bg-white px-6 py-2.5 text-sm font-semibold text-black transition hover:bg-[#70482F] active:scale-95"
+              className="rounded-3xl bg-white px-6 py-2.5 text-sm font-semibold text-black transition hover:bg-[#70482F] uppercase active:scale-95"
             >
               Shop Now
             </Link>
@@ -43,9 +44,9 @@ function HeroSection({ onViewCategories }) {
             <button
               type="button"
               onClick={onViewCategories}
-              className="rounded-3xl border border-white/50 bg-transparent px-7 py-2.5 text-sm font-semibold text-white transition hover:bg-white/45 active:scale-95"
+              className="rounded-3xl border border-white/50 bg-transparent px-7 py-2.5 text-sm font-semibold text-white transition hover:bg-white/45 uppercase active:scale-95"
             >
-              VIEW CATEGORIES
+              View Categories
             </button>
           </div>
 
