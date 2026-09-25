bd18a01629d644ed2de4940bab003a4b99a5be53
Ahmed <ahmednage2024@gmail.com>
2026-09-25 22:05:26 +0300

    store-final-deploy-V1.1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://e-commerce-api-3wara.vercel.app/",
+  baseURL: "/api-proxy",
   withCredentials: true
 });
 // Request Interceptor
